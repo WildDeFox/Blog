b70dd6363139402de824fa6c2f74b952c74ceb73
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-18 22:18:19 +0700

    Реализация класса name

--- src/Person/Name.php
+++ src/Person/Name.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Blog\Defox\Person;
+
+class Name
+{
+    private string $firstName;
+    private string $lastName;
+
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function __toString(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
