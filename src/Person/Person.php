5a745429bacaee37837753b0f9b9eaa37b5eb30f
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-18 22:28:20 +0700

    Реализация класса Рerson

--- src/Person/Person.php
+++ src/Person/Person.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Blog\Defox\Person;
+
+use DateTimeImmutable;
+
+class Person
+{
+    private DateTimeImmutable $registeredOn;
+    private Name $name;
+
+    public function __construct(Name $name, DateTimeImmutable $registeredOn)
+    {
+        $this->name = $name;
+        $this->registeredOn = $registeredOn;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name . ' на сайте с (' . $this->registeredOn->format('Y-m-d') . ')';
+    }
+}
