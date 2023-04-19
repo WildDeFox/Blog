d10ec1aae93a193acca59e47bfa8213bdc4b3a0a
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-19 22:04:24 +0700

    Реализация класса User

--- cli.php
+++ cli.php
@@ -1,1 +1,11 @@
 <?php
+
+use Blog\Defox\Blog\User;
+use Blog\Defox\Person\Name;
+
+include __DIR__ . '/vendor/autoload.php';
+
+$name = new Name('Никита', 'Капурин');
+$user = new User(1, $name);
+echo $user;
+echo $name;
--- src/Blog/User.php
+++ src/Blog/User.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Blog\Defox\Blog;
+
+use Blog\Defox\Person\Name;
+
+class User
+{
+    private int $id;
+    private Name $name;
+
+    public function __construct(int $id, Name $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function __toString(): string
+    {
+        return "Пользователь $this->name с id:$this->id";
+    }
+}
