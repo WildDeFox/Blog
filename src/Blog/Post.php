dfdf5ab12426061dc0d491010b0f77051be3b16c
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-25 22:07:20 +0700

    Post: добавлены геттеры и сеттеры

--- src/Blog/Post.php
+++ src/Blog/Post.php
@@ -15,6 +15,56 @@ class Post
         $this->text = $text;
     }
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
+
+
     public function __toString(): string
     {
         return "Пользователь $this->user оставил пост: $this->text, id:$this->id";
