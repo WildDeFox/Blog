dec7e0d39e7d1eba775748d872944839bc17f361
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-29 16:51:11 +0700

    Рефакторинг класса User

--- src/Blog/User.php
+++ src/Blog/User.php
@@ -6,13 +6,31 @@ use Blog\Defox\Person\Name;
 
 class User
 {
-    private int $id;
+    private UUID $uuid;
     private Name $name;
+    private string $username;
 
-    public function __construct(int $id, Name $name)
+    public function __construct(UUID $uuid, Name $name, string $username)
     {
-        $this->id = $id;
+        $this->uuid = $uuid;
         $this->name = $name;
+        $this->username = $username;
+    }
+
+    /**
+     * @return UUID
+     */
+    public function getUuid(): UUID
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @param UUID $uuid
+     */
+    public function setUuid(UUID $uuid): void
+    {
+        $this->uuid = $uuid;
     }
 
     /**
@@ -31,15 +49,24 @@ class User
         $this->name = $name;
     }
 
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
 
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
 
     public function __toString(): string
     {
-        return "Пользователь $this->name с id:$this->id";
-    }
-
-    public function getId(): string
-    {
-        return $this->id;
+        return "Пользователь $this->name с uuid:$this->uuid";
     }
 }
